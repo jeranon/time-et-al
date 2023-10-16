e2b8b3bda57ceaa2902c346367f05589f1da4e91
Jeranon <jeranon@gmail.com>
2023-10-16 07:24:42 -0600

    Removed .gitkeep powershell script, as we have nailed down a better process to retain necessary (empty) folders using the setup script.

--- gitkeep.ps1
+++
@@ -1,8 +0,0 @@
-# Get the path to your project directory
-$projectDir = "O:\time_app"
-
-# Find all empty directories within the project directory
-$emptyDirs = Get-ChildItem -Path $projectDir -Recurse -Directory | Where-Object { (Get-ChildItem -Path $_.FullName).Count -eq 0 }
-
-# For each empty directory, create a .gitkeep file inside it
-$emptyDirs | ForEach-Object { New-Item -Path $_.FullName -Name ".gitkeep" -ItemType "file" -Force }
